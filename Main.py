6db5753364eef61296f4c1c029db4e487b531752
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-07-31 00:52:06 +0530

    Initial commit for code template.

---
+++ Main.py
@@ -0,0 +1,55 @@
+class MyCircularQueue:
+    def __init__(self, size: int):
+        # Write code here
+
+    def enqueue(self, value: int) -> bool:
+        # Write code here
+
+    def dequeue(self) -> bool:
+        # Write code here
+
+    def get_front(self) -> int:
+        # Write code here
+
+    def get_rear(self):
+        # Write code here
+
+    def is_empty(self):
+        # Write code here
+
+    def is_full(self):
+        # Write code here
+
+
+# Do not change the following code
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+print(operations)
+data = []
+for item in input().split(','):
+    item = item.strip()
+    if item == '-':
+        data.append([])
+    else:
+        data.append([int(item)])
+print(data)
+obj = MyCircularQueue(data[0][0])
+result = []
+for i in range(len(operations)):
+    if i == 0:
+        result.append(None)
+    elif operations[i] == "enqueue":
+        result.append(obj.enqueue(data[i][0]))
+    elif operations[i] == "get_rear":
+        result.append(obj.get_rear())
+    elif operations[i] == "get_front":
+        result.append(obj.get_front())
+    elif operations[i] == "dequeue":
+        result.append(obj.dequeue())
+    elif operations[i] == "is_full":
+        result.append(obj.is_full())
+    elif operations[i] == "is_empty":
+        result.append(obj.is_empty())
+
+print(result)
